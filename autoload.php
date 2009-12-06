ca80b4046454cbaffc029fe89de956d1c92b46e8
Marek Dajnowski <fornve@yahoo.co.uk>
2009-12-06 20:36:17 +0000

    initial

--- autoload.php
+++ autoload.php
@@ -7,7 +7,7 @@
 defined('_JEXEC') or die('Restricted access');
 
 // DAO base class (what is DAO? http://en.wikipedia.org/wiki/Data_access_object)
-JLoader::register( 'Entity', JPATH_BASE .'/libraries/entities/entity.class.php' );
+JLoader::register( 'Entity', JPATH_BASE .'/libraries/dao/entity.class.php' );
 
-JLoader::register( 'Category', JPATH_BASE .'/libraries/entities/category.class.php' );
-JLoader::register( 'Item', JPATH_BASE .'/libraries/entities/item.class.php' );
+JLoader::register( 'Category', JPATH_BASE .'/libraries/dao/entities/category.class.php' );
+JLoader::register( 'Item', JPATH_BASE .'/libraries/dao/entities/item.class.php' );
